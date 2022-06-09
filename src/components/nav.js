c2e5e7090efae11920187a6f0ad72253211ff9d6
lennoncc <lencruz@ucdavis.edu>
2022-06-08 23:28:55 -0700

    some changes

--- src/components/nav.js
+++ src/components/nav.js
@@ -173,7 +173,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="https://drive.google.com/file/d/1v_7Nh7uFMCI3diCi8FejzB9SxXHJcZXi/view?usp=sharing" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
